a35af612b824dcfc6b9d0cad10d98fe4cbf06df7
tejas parakh <tparakh7@gmail.com>
2020-06-09 20:25:45 +0530

    done4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>Hello angular..!</h1>'
 })
 export class AppComponent {
-  title = 'NgProjectX';
+ 
 }
