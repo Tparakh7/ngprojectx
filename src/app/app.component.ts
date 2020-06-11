9c1274fc5a0f9f11c1c91327df139924e925d90a
tejas parakh <tparakh7@gmail.com>
2020-06-11 20:15:08 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,16 +12,19 @@ export class AppComponent {
      bikecompany:'Yamaha',
      bikename:'R15 V2',
      topspeed:'135km/hr',
+     instock:5,
    },
    {
      bikecompany:'Bajaj',
      bikename:'NS200',
      topspeed:'145km/hr',
+     instock:7,
    },
    {
      bikecompany:'TVS',
      bikename:'Apache RR310',
      topspeed:'160km/hr',
+     instock:0,
    }
 
  ]
